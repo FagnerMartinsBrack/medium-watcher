7e05a3941f1195f3a88d5754b671f7476e146c76
Fagner Brack <github@fagnermartins.com>
2023-04-24 16:39:21 +1000

    Send empty String to LinkedIn API if a cover image is not found

--- src/post-details/query-cover-img-link.mjs
+++ src/post-details/query-cover-img-link.mjs
@@ -4,6 +4,5 @@ const parseHTML = (html) => new JSDOM(html).window.document;
 
 export default (html) => {
   const coverImageElement = parseHTML(html).querySelectorAll('img')[0];
-  if (!coverImageElement) throw new Error('No cover image found');
-  return coverImageElement.getAttribute('src');
+  return coverImageElement ? coverImageElement.getAttribute('src') : '';
 };
--- src/post-details/query-cover-img-link.spec.js
+++ src/post-details/query-cover-img-link.spec.js
@@ -1,8 +1,16 @@
 import { expect } from 'chai';
 import queryCoverImgLink from './query-cover-img-link.mjs';
 
-it('queries cover image link', async () => {
-  expect(
-    queryCoverImgLink('<img src="https://cdn-images-1.medium.com/max/820/1*rA_N7T1yq6aXTjBQE4D8Wg.jpeg">')
-  ).to.eql('https://cdn-images-1.medium.com/max/820/1*rA_N7T1yq6aXTjBQE4D8Wg.jpeg');
+describe('Query Cover Image Link', () => {
+  it('queries cover image link', async () => {
+    expect(
+      queryCoverImgLink('<img src="https://cdn-images-1.medium.com/max/820/1*rA_N7T1yq6aXTjBQE4D8Wg.jpeg">')
+    ).to.eql('https://cdn-images-1.medium.com/max/820/1*rA_N7T1yq6aXTjBQE4D8Wg.jpeg');
+  });
+
+  it('returns empty String if a cover image link could not be found', async () => {
+    expect(
+      queryCoverImgLink('any <b>html</b> without a <p>cover image link</p>')
+    ).to.eql('');
+  });
 });
