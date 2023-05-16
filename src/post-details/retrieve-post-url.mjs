d4237cfb9f1413441e4922a29ffdab59e01866a1
Fagner Brack <github@fagnermartins.com>
2023-05-16 20:46:48 +1000

    Remove log of the config when retrieving the post URL
    It's a secret, so Github Actions will ubfuscate the value anyway

--- src/post-details/retrieve-post-url.mjs
+++ src/post-details/retrieve-post-url.mjs
@@ -1,5 +1,4 @@
 export default (feedItem, config) => {
-  console.log('Found config when retrieving post URL', config);
   if (config.replace_feed_item_link_domain) {
     const components = config.replace_feed_item_link_domain.split(',');
     if (components.length === 2) {
