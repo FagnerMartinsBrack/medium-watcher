4dd31df0e452064cf8adad404d0e1d314e63e48d
Fagner Brack <github@fagnermartins.com>
2023-05-16 20:40:07 +1000

    Add log for config when retrieving post URL

--- src/post-details/retrieve-post-url.mjs
+++ src/post-details/retrieve-post-url.mjs
@@ -1,4 +1,5 @@
 export default (feedItem, config) => {
+  console.log('Found config when retrieving post URL', config);
   if (config.replace_feed_item_link_domain) {
     const components = config.replace_feed_item_link_domain.split(',');
     if (components.length === 2) {
