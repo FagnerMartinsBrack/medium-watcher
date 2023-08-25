31b1f3ec7de95119ff11d12ec8cd4c668455fa37
Fagner Brack <github@fagnermartins.com>
2023-08-25 14:30:24 +1000

    Increase timeout for shouldNotify() tests due to timeout issue in CI

--- src/should-notify.spec.js
+++ src/should-notify.spec.js
@@ -4,13 +4,13 @@ import shouldNotify from './should-notify.mjs';
 describe('Should notify subscribers of a new post?', () => {
   it('DOES NOT notify if running many hours after the last post', () => {
     expect(shouldNotify({ lastPostDate: '2023-04-22 08:01', nowDate: '2023-04-22T18:01:00.000Z' })).to.eql(false);
-  });
+  }).timeout(10000);
 
   it('notifies if there\'s a new post in the last hour from execution of the workflow', () => {
     expect(shouldNotify({ lastPostDate: '2023-04-22T05:30:00.000Z', nowDate: '2023-04-22T06:29:00.000Z' })).to.eql(true);
-  });
+  }).timeout(10000);
 
   it('notifies if there\'s a new post exactly one hour from execution of the workflow', () => {
     expect(shouldNotify({ lastPostDate: '2023-04-22T05:30:00.000Z', nowDate: '2023-04-22T06:30:00.000Z' })).to.eql(true);
-  });
+  }).timeout(10000);
 });
